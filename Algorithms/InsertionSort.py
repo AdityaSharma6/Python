f89cc0a485dd13771758220fe21edbb2b6d37047
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-05-25 18:45:22 -0400

    Update InsertionSort.py
    Condensed Code

--- Algorithms/InsertionSort.py
+++ Algorithms/InsertionSort.py
@@ -2,16 +2,10 @@ def insertionSort(array):
     for i in range(len(array)):
         futureValue = array[i]
         j = i - 1
-        while (j >= 0):
-            if futureValue < array[j]:
-                array[j+1] = array[j]
-                array[j] = futureValue
-                j = j - 1
-            else:
-                break
-    
+        while ((j >= 0) and (futureValue < array[j])): 
+            array[j+1] = array[j]
+            array[j] = futureValue
+            j = j - 1
     print(array)
             
-            
-            
-insertionSort([8,3,6,0,2,6,4,0,1])
+insertionSort([8,7,2,4,9,3,6,0,1,6,5])
