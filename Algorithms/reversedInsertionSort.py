c122f9ac18f32a7a5e8114fe0f552b2a1878581a
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-05-25 18:46:50 -0400

    Add files via upload
    Insertion Sort in reversed order

---
+++ Algorithms/reversedInsertionSort.py
@@ -0,0 +1,11 @@
+def reversedInsertionSort(array):
+    for i in range(len(array)):
+        futureValue = array[i]
+        j = i - 1
+        while ((j>=0) and (futureValue > array[j])):
+                array[j+1] = array[j]
+                array[j] = futureValue
+                j -= 1
+    print(array)
+
+reversedInsertionSort([8,7,2,4,9,3,6,0,1,6,5])
